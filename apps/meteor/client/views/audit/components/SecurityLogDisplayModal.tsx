4456c00eb2f9adc048da975f6955585edd55b675
MartinSchoeler <martinschoeler8@gmail.com>
2025-02-25 17:11:24 -0300

    fix: use Generic modal

--- apps/meteor/client/views/audit/components/SecurityLogDisplayModal.tsx
+++ apps/meteor/client/views/audit/components/SecurityLogDisplayModal.tsx
@@ -1,10 +1,11 @@
-import { Box, Modal } from '@rocket.chat/fuselage';
+import { Box } from '@rocket.chat/fuselage';
 import { UserAvatar } from '@rocket.chat/ui-avatar';
 import { useSettingStructure } from '@rocket.chat/ui-contexts';
 import moment from 'moment';
 import { useTranslation } from 'react-i18next';
 
 import { CodeDisplay } from './CodeDisplay';
+import GenericModal from '../../../components/GenericModal';
 import { InfoPanelLabel, InfoPanelText } from '../../../components/InfoPanel';
 
 type SecurityLogDisplayProps = {
@@ -20,37 +21,36 @@ export const SecurityLogDisplay = ({ timestamp, actor, setting, changedFrom, cha
 	const { t } = useTranslation();
 	const settingStructure = useSettingStructure(setting);
 	return (
-		<Modal>
-			<Modal.Header>
-				<Modal.Title>{t('Setting_change')}</Modal.Title>
-				<Modal.Close onClick={onCancel} />
-			</Modal.Header>
-			<Modal.Content mbe={16}>
-				<InfoPanelLabel>{t('Timestamp')}</InfoPanelLabel>
-				<InfoPanelText>{moment(timestamp).format('MMMM Do YYYY, h:mm:ss a')}</InfoPanelText>
+		<GenericModal
+			icon={null}
+			// confirmText={t('Add')}
+			onClose={onCancel}
+			title={t('Setting_change')}
+		>
+			<InfoPanelLabel>{t('Timestamp')}</InfoPanelLabel>
+			<InfoPanelText>{moment(timestamp).format('MMMM Do YYYY, h:mm:ss a')}</InfoPanelText>
 
-				<InfoPanelLabel>{t('Actor')}</InfoPanelLabel>
+			<InfoPanelLabel>{t('Actor')}</InfoPanelLabel>
 
-				<Box display='flex' alignItems='center' mbe={16}>
-					<UserAvatar size='x24' username={actor} />
-					<Box display='flex' withTruncatedText mi={8}>
-						<Box display='flex' flexDirection='column' alignSelf='center' withTruncatedText>
-							<Box fontScale='p2m' withTruncatedText color='default'>
-								{actor}
-							</Box>
+			<Box display='flex' alignItems='center' mbe={16}>
+				<UserAvatar size='x24' username={actor} />
+				<Box display='flex' withTruncatedText mi={8}>
+					<Box display='flex' flexDirection='column' alignSelf='center' withTruncatedText>
+						<Box fontScale='p2m' withTruncatedText color='default'>
+							{actor}
 						</Box>
 					</Box>
 				</Box>
+			</Box>
 
-				<InfoPanelLabel>{t('Setting')}</InfoPanelLabel>
-				<InfoPanelText>{t(setting)}</InfoPanelText>
+			<InfoPanelLabel>{t('Setting')}</InfoPanelLabel>
+			<InfoPanelText>{t(setting)}</InfoPanelText>
 
-				<InfoPanelLabel>{t('Changed_from')}</InfoPanelLabel>
-				{settingStructure?.type === 'code' ? <CodeDisplay code={changedFrom} /> : <InfoPanelText>{changedFrom}</InfoPanelText>}
+			<InfoPanelLabel>{t('Changed_from')}</InfoPanelLabel>
+			{settingStructure?.type === 'code' ? <CodeDisplay code={changedFrom} /> : <InfoPanelText>{changedFrom}</InfoPanelText>}
 
-				<InfoPanelLabel>{t('Changed_to')}</InfoPanelLabel>
-				{settingStructure?.type === 'code' ? <CodeDisplay code={changedTo} /> : <InfoPanelText>{changedTo}</InfoPanelText>}
-			</Modal.Content>
-		</Modal>
+			<InfoPanelLabel>{t('Changed_to')}</InfoPanelLabel>
+			{settingStructure?.type === 'code' ? <CodeDisplay code={changedTo} /> : <InfoPanelText>{changedTo}</InfoPanelText>}
+		</GenericModal>
 	);
 };
